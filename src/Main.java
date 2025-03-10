1cd03f8f7c67920d28b93cda87bae3db36296728
KOVS194 <ovsyasha16@gmail.com>
2025-03-10 08:34:05 +0100

    init commit to develop branch

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
+import Simulator.Simulator;
+
 public class Main {
     public static void main(String[] args) {
+        Simulator simulator = new Simulator("Real exam");
+
+        System.out.println(simulator.getName());
  }
 }
---
+++ src/Simulator/Simulator.java
@@ -0,0 +1,13 @@
+package Simulator;
+
+public class Simulator {
+    private String name;
+
+    public Simulator(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
